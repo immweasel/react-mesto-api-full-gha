6edac24b585b6c5f88248887614c7707949f2780
Кира Максимова 211-323 <immweasel@yandex.ru>
2023-10-06 20:16:32 +0300

    минимальные изменения, проверка секретного ключа

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-
+// console.log(process.env)
 const app = express();
 
 app.use(cors());
